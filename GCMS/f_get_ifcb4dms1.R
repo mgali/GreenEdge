c5b0660794867975ad7f1f3ea3d5d0fa1536d9da
martigalitapias <marti.gali.tapias@gmail.com>
2019-08-28 13:34:53 +0200

    Work in progress on ifcb

---
+++ GCMS/f_get_ifcb4dms1.R
@@ -0,0 +1,43 @@
+# FUNCTION TO EXTRACT DESIRED INFORMATION FROM IFCB FILES FROM GREENEDGES
+# Input: ifcb sample file as formatted by Philippe-Israel Morin
+#
+# Output: data frame with as many columns as taxonomic levels targetted for data aggregation,
+# plus Phaeocystis counts. Currently length(out) = 10.
+#
+# Martí Galí August 2019
+
+f_get_ifcb4dms1 <- function(y) {
+  
+  # Define taxonomic groups to be extracted
+  g <- list()
+  g$detritus_mg_L <- 'detritus (not living)'
+  g$diat_pelagic_mg_L <- c('centric (Bacillariophyta)',
+                      'Thalassiosira (Mediophyceae)',
+                      'Chaetoceros (Mediophyceae)',
+                      'pennate (Bacillariophyta)',
+                      'Thalassiosira nordenskioeldii (Thalassiosira)',
+                      'Porosira (Mediophyceae)',
+                      'Eucampia (Mediophyceae)',
+                      'Bacillariophyta (Ochrophyta)',
+                      'Attheya (Mediophyceae)',
+                      'Cylindrotheca closterium (Cylindrotheca)',
+                      'Pseudo-nitzschia (Bacillariophyceae)',
+                      'Nitzschia frigida (Nitzschia)',
+                      'chain (centric)',
+                      'chain (pennate)',
+                      'Entomoneis (Bacillariophyceae)',
+                      'Polarella glacialis (Polarella)')
+  g$melo_mg_L <- 'Melosira (Melosirids)'
+  g$prym_mg_L <- 'Prymnesiophyceae (Haptophyta)'
+  g$phaeo_mg_L <- 'Phaeocystis (Phaeocystaceae)'
+  g$prym_clumped_mg_L <- 'clumped Prymnesiophyceae'
+  g$dino_mg_L <- 'Dinophyceae (Holodinophyta)'
+  g$badfocus_mg_L <- 'badfocus (artefact)'
+  
+  out <- lapply(g, function(x) sum(y$mgC_per_L[y$object_annotation_category %in% x], na.rm = T))
+  out <- as.data.frame(t(unlist(out)))
+  out$tot_mg_L <- sum(y$mgC_per_L, na.rm = T)
+  out$phaeocystis_n_mL <- sum(y$mgC_per_L[y$object_annotation_category %in% 'Phaeocystis (Phaeocystaceae)'], na.rm = T)
+  return(out)
+  
+}
--- GCMS/process_IFCB_matchDMS.R
+++ GCMS/process_IFCB_matchDMS.R
@@ -2,6 +2,7 @@
 # ONLY MEASUREMENTS MATCHING DMS(P) DURING GREEN EDGE CRUISE LEG 1B
 
 library(dplyr)
+source('~/Desktop/GreenEdge/GCMS/f_get_ifcb4dms1.R')
 
 # Set general data path
 genpath <- '~/Desktop/GreenEdge/GCMS/'
@@ -25,28 +26,58 @@ colnames(ifcb.log) <- nn
 ifcb.log <- ifcb.log[order(ifcb.log$cast, ifcb.log$depth),]
 
 # ------------------------------------------------------------------------
+# Define names of ifcb variables exported in next loop
+# IMPORTANT: update this vector to match categories in f_get_ifcb4dms1.R, if changed
+names_ifcb <- c("detritus_mg_L","diat_pelagic_mg_L","melosira_mg_L","prym_mg_L","phaeocystis_mg_L",
+                "prym_clumped_mg_L","dino_mg_L","badfocus_mg_L","phaeocystis_n_mL")
+
+# Preallocate ifcb data matching prof.all
+match_ifcb <- data.frame(NA, dim = c(dim(prof.all[1]),length(names_ifcb)))
+colnames(match_ifcb) <- names_ifcb
+
+# ------------------------------------------------------------------------
 # Loop through casts and depths and match to IFCB
 cast <- unique(prof.all$cast)
 for (jc in cast) {
   
-  # Subset prof.all data and find IFCB file match (merge)
+  # Subset prof.all data [and find IFCB file match (merge): avoided because of variable n of files per depth]
   tmp.cast <- subset.data.frame(prof.all[prof.all$cast == jc,], select = c(cast,depth))
   tmp.cast$depth[tmp.cast$depth == 0.7] <- 0  # Set 0.7 to 0 m depth for exact matching of IFCB
-  tmp.cast <- merge(x = tmp.cast, y = ifcb.log, by = c('cast','depth'), all.x = T, all.y = F)
-  # tmp.cast <- dplyr::left_join(x = tmp.cast, ifcb.log) # equivalent
+  # tmp.cast <- merge(x = tmp.cast, y = ifcb.log, by = c('cast','depth'), all.x = T, all.y = F)
+  # tmp.cast <- dplyr::left_join(x = tmp.cast, ifcb.log) # equivalent to merge
   
-  # Loop through ifcb filenames in cast (match unique names, then expand for repeated depths)
-  cfile <- unique(tmp.cast$File_Name)
-  for (jf in cfile) {
+  # Loop through depths in cast (match unique names, then expand for repeated depths)
+  cdepth <- unique(tmp.cast$depth)
+  for (jd in cdepth) {
     
-    if (!is.na(jf)) {
-      fpath <- paste0(ifcbpath,'post_processing/export_144_20190111_1439/processed_counts/',
-                      jf,'.csv')
-      ifcb.data <- read.csv(fpath, header = T)
-      print(ifcb.data)
-    }
-  }
+    # Preallocate here dataframe of dim [length(cdepth) * length(jf_data)]
+    
+    # Search all filenames
+    cdfiles <- unique(ifcb.log$File_Name[ifcb.log$depth == jd])
+    cdfiles <- cdfiles[!is.na(cdfiles)]
+    if (!is_empty(cdfiles)) {
+      
+      # Preallocate here dataframe of dim [length(cdfiles) * length(jf_data)]
+      tmp.cdfiles <- list()
+      
+      for (jf in cdfiles){
+        fpath <- paste0(ifcbpath,'post_processing/export_144_20190111_1439/processed_counts/',
+                        jf,'.csv')
+        if (file.exists(fpath)) {
+          ifcb.data <- read.csv(fpath, header = T)
+          jf_data <- f_get_ifcb4dms1(ifcb.data)
+          tmp.cdfiles[[ as.character(jf) ]] <- jf_data
+        } # test file exists
+      } # loop on file names within depth
+      
+      # Convert to df and sum columns
+      tmp.cdfiles <- do.call("rbind",tmp.cdfiles) # elegant. Line below does the same without getting the col names, and less elegantly
+      # tmp.cdfiles <- data.frame(matrix(unlist(tmp.cdfiles), nrow=length(tmp.cdfiles), byrow=T))
+      sum.cdfiles <- colSums(tmp.cdfiles)
+      
+    } # test non-empty list of files
+  } # loop on depths within cast
   
   # print(tmp.cast)
   
-}
+} # loop on casts
