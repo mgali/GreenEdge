e6de5d88cc18b0875f3004e64dfbc495bd944723
martigalitapias <marti.gali.tapias@gmail.com>
2020-02-19 12:18:03 +0100

    Refining DMSP-phyto biomass estimates

--- MS_DMS_GE16_Elementa/Fig_phyto/DMSP_partition_phyto_v2.R
+++ MS_DMS_GE16_Elementa/Fig_phyto/DMSP_partition_phyto_v2.R
@@ -1,6 +1,7 @@
 # MAKE PLOTS OF DMS AND DMSP VS. PHYTO COUNTS AND ADDITIONAL Z VARIABLES
 
 library(dplyr)
+library(tidyr)
 library(RColorBrewer)
 library(classInt) # for function classIntervals
 # Check http://geog.uoregon.edu/GeogR/topics/multiplots01.html
@@ -42,7 +43,7 @@ prof <- prof[prof$depth!=0.1 & !is.na(prof$prescreen) ,]
 
 # Add surface and SCM categories
 prof$scm <- 'SCM'
-prof$scm[prof$depth <= 10] <- 'surface'
+prof$scm[prof$depth < 10] <- 'surface'
 
 # Rename DMS and remove unnecessary variables
 prof$dms <- prof$dms_consens_cf68
@@ -77,15 +78,15 @@ esd <- list(dino_athec = 15,
             crypt = 5,
             Phaeo = 5,
             flag = 5)
-rdmsp <- list(dino_athec = 0.10,
-              dino_thec = 0.10,
-              diat_cen = 0.02,
-              diat_pen = 0.02,
+rdmsp <- list(dino_athec = 0.05,
+              dino_thec = 0.05,
+              diat_cen = 0.01,
+              diat_pen = 0.01,
               chrys = 0.10,
               crypt = 0.02,
               Phaeo = 0.10,
               flag = 0.10,
-              diat_all = 0.02)
+              diat_all = 0.01)
 
 for (nn in names(esd)) {
   if (nn %in% c("diat_cen","diat_pen")) {
@@ -106,72 +107,101 @@ for (nn in names(esd)) {
 prof$dmspt.diat_Laf.Cmic <- prof$diat_Laf.mgC_L.mic * rdmsp$diat_all * (1/12) * (1/5) * 1e6
 prof$dmspt.diat_Laf.Cmic.pc <- 100 * prof$dmspt.diat_Laf.Cmic / (fp * prof$dmspt)
 
-# Remove microscopy counts-based data
+# Remove microscopy counts-based data for diatoms
 prof <- select(prof, -c(diat_cen.mgC_L.mic, dmspt.diat_cen.Cmic, dmspt.diat_cen.Cmic.pc, diat_pen.mgC_L.mic, dmspt.diat_pen.Cmic, dmspt.diat_pen.Cmic.pc))
-
-# Add sum of percentages
-prof$sum.pc <- rowSums(prof[ , grepl(".pc",names(prof))])
-# View(prof$sum.pc)
 
 # OUTPUT
 OUT <- prof
 # View(OUT)
 write.csv(x = OUT, file = paste0(opath,"Fraction_DMSPt_phyto.csv"), row.names = F)
 
-
 # ---------------------
 # Plot
 
-phynames <- list("dmspt.diat_Laf.Cmic"="Diat_all",
-                 "dmspt.diat_cen.Cmic"="Diat_C",
-                 "dmspt.diat_pen.Cmic"="Diat_P",
-                 "dmspt.dino_athec.Cmic"="Dino_A",
-                 "dmspt.dino_thec.Cmic"="Dino_T",
-                 "dmspt.chrys.Cmic"="Chryso",
-                 "dmspt.crypt.Cmic"="Crypto",
-                 "dmspt.Phaeo.Cmic"="Phaeocystis",
-                 "dmspt.flag.Cmic"="Flag_other")
-# dfplot <- 
+parcol <- brewer.pal(8, 'Dark2')
+
+phynames <- list("dmspt.diat_Laf.Cmic.pc"="Diat_all",
+                 "dmspt.diat_cen.Cmic.pc"="Diat_C",
+                 "dmspt.diat_pen.Cmic.pc"="Diat_P",
+                 "dmspt.dino_athec.Cmic.pc"="Dino_A",
+                 "dmspt.dino_thec.Cmic.pc"="Dino_T",
+                 "dmspt.chrys.Cmic.pc"="Chryso",
+                 "dmspt.crypt.Cmic.pc"="Crypto",
+                 "dmspt.Phaeo.Cmic.pc"="Phaeocystis",
+                 "dmspt.Phaeo.counts.pc"="Phaeocystis_cq",
+                 "dmspt.flag.Cmic.pc"="Flag_other")
+dfplot <-prof[ , c( "stn","depth","scm","dmspt",names(prof)[grepl("dmspt.", names(prof)) & grepl(".pc", names(prof))] )]
+for (nn in names(phynames)) {names(dfplot)[names(dfplot) == nn] <- phynames[[nn]]}
+
+# # View sum of percentages
+# sums <- data.frame('stn' = dfplot[dfplot$scm=="surface", "stn"],
+#                    'surface' = rowSums(dfplot[dfplot$scm=="surface", 6:dim(dfplot)[2]]),
+#                    'SCM' = rowSums(dfplot[dfplot$scm=="SCM", 6:dim(dfplot)[2]]))
+# View(sums)
+
+# # TidyR format
+# gather(prof[ ,grepl("dmspt.", names(prof)) & !grepl(".pc", names(prof))])
 
 
-# if (exportimg) {png(filename = paste0(opath,"dms_dmspt_",pg,"_counts.png"), width = 6, height = 7, units = 'cm', pointsize = 8, bg = 'white', res = 600, type = 'cairo')}
+if (exportimg) {png(filename = paste0(opath,"dmspt_biomass.png"), width = 16, height = 12, units = 'cm', pointsize = 8, bg = 'white', res = 600, type = 'cairo')}
 
 # Multipanel setup
-# m <- rbind(matrix(data = 1, nrow = 4, ncol = 9), matrix(data = 2, nrow = 4, ncol = 9))
-# layout(m)
-# par(oma = c(1,1,0.5,0.5))
+m <- rbind(matrix(data = 1, nrow = 4, ncol = 9), matrix(data = 2, nrow = 4, ncol = 9))
+layout(m)
+par(oma = c(1,1,0.5,0.5))
 
 # ---------------------
 # a) DMSPp partition surface
+par(mar = c(5,5,2,2))
 
+barplot(height = t(as.matrix(dfplot[dfplot$scm=="surface",6:dim(dfplot)[2]])),
+        names.arg = rep("",9),
+        beside = F,
+        legend.text = names(dfplot)[6:dim(dfplot)[2]],
+        args.legend = list(x = "right", bty = "n", cex = 1.2),
+        col = parcol,
+        border = F,
+        ylab = "% of DMSPp",
+        axes = F,
+        ylim = c(0,100),
+        cex = 1.2,
+        cex.names = 1.2,
+        cex.lab = 1.2,
+        las = 1,
+        main = "a) Surface")
+mtext(text = as.character(round(dfplot[dfplot$scm=="surface", "dmspt"], digits = 0)),
+      side = 3,
+      line = -3,
+      at = seq(0.6, 0.6+8*1.2, 1.2),
+      cex = 0.8,
+      font = 2)
+axis(2, labels = T, cex.axis = 1.2)
 
 # ---------------------
 # b) DMSPp partition SCM
+par(mar = c(5,5,2,2))
 
+barplot(height = t(as.matrix(dfplot[dfplot$scm=="SCM",6:dim(dfplot)[2]])),
+        names.arg = dfplot$stn[dfplot$scm=="SCM"],
+        beside = F,
+        col = parcol,
+        border = F,
+        ylab = "% of DMSPp",
+        axes = F,
+        ylim = c(0,100),
+        cex = 1.2,
+        cex.names = 1.2,
+        cex.lab = 1.2,
+        las = 1,
+        main = "b) Subsurface chlorophyll maximum (SCM)",
+        xlab = "Station")
+mtext(text = as.character(round(dfplot[dfplot$scm=="SCM", "dmspt"], digits = 0)),
+      side = 3,
+      line = -3,
+      at = seq(0.6, 0.6+8*1.2, 1.2),
+      cex = 0.8,
+      font = 2)
+axis(2, labels = T, cex.axis = 1.2)
 
-# if (exportimg) {dev.off()}
+if (exportimg) {dev.off()}
 
-
-
-# --------------------------
-# OLD
-# prof$Phaeo_mgC_L.microscopy <- MendenDeuer_Lessard(esd_microm = 5, cell_conc = prof$Phaeo, is_diatom = F) * 1e-9
-# rdmsp.phaeo <- 0.10
-# prof$dmspt_phaeo.Cmic <- prof$Phaeo_mgC_L.microscopy * rdmsp.phaeo * (1/12) * (1/5) * 1e6
-# prof$dmspt_phaeo.Cmic.pc <- 100 * prof$dmspt_phaeo.Cmic / (fp * prof$dmspt)
-# print(summary(prof$dmspt_phaeo.Cmic.pc))
-# 
-# prof$diat_cen_mgC_L.microscopy <- MendenDeuer_Lessard(esd_microm = 100, cell_conc = prof$diat_cen, is_diatom = T) * 1e-9
-# prof$diat_pen_mgC_L.microscopy <- MendenDeuer_Lessard(esd_microm = 20, cell_conc = prof$diat_pen, is_diatom = T) * 1e-9
-# prof$diat_mgC_L.microscopy <- prof$diat_cen_mgC_L.microscopy + prof$diat_pen_mgC_L.microscopy
-# rdmsp.diat <- 0.02
-# prof$dmspt_diat.Cmic <- prof$diat_mgC_L.microscopy * rdmsp.diat * (1/12) * (1/5) * 1e6
-# prof$dmspt_diat.Cmic.pc <- 100 * prof$dmspt_diat.Cmic / (fp * prof$dmspt)
-# print(summary(prof$dmspt_diat.Cmic.pc))
-
-# OUT <- prof[,c("stn","depth",
-#                "Phaeo","diat_cen","diat_pen",
-#                "Phaeo_mgC_L.microscopy","diat_cen_mgC_L.microscopy","diat_pen_mgC_L.microscopy","diat_mgC_L.microscopy",
-#                "dmspt.Phaeo.counts","dmspt_phaeo.Cmic","dmspt_diat.Cmic",
-#                "dmspt",
-#                "dmspt.Phaeo.counts.pc","dmspt_phaeo.Cmic.pc","dmspt_diat.Cmic.pc")]
