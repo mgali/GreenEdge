d8eed93cae0dfdf9c3355a581ff85dcfe5eb6725
martigalitapias <marti.gali.tapias@gmail.com>
2020-02-19 18:38:41 +0100

    Refining DMSP-phyto biomass estimates

--- MS_DMS_GE16_Elementa/Fig_phyto/DMSP_partition_phyto_v2.R
+++ MS_DMS_GE16_Elementa/Fig_phyto/DMSP_partition_phyto_v2.R
@@ -15,16 +15,29 @@ diatL$diat_Laf.mgC_L.mic <- diatL$diat_Laf.mgC_L.mic/1000 # Correct units
 # Exporting image?
 exportimg <- T
 opath <- "~/Desktop/GreenEdge/MS_DMS_GE16_Elementa/Fig_phyto/"
+ymax <- 120
+
+# Use Cdmspt:Ctot ratio or intracellular concentration?
+cmethod <- "ratio" # choose ratio or intraconc
+
+addcd <- "" # default. Detritus and Phaeo colonies only allowed if using ratios
+if (cmethod == "ratio"){
+  # Add Phaeo colonies and detritus?
+  addcd <- "" # leave empty or put _PhaeoCol_detr
+}
+
+# Stefels2007 ratios? Does not apply if using quotas
+Stefels <- "" # leave empty or put _Stefels
+if(cmethod == "intraconc") {Stefels <- ""}
 
 # Function to compute carbon content of phytoplankton
-MendenDeuer_Lessard <- function(esd_microm, cell_conc, is_diatom) {
-  vol <- (4/3) * pi * ((esd_microm/2)^3) # µm^3
+MendenDeuer_Lessard <- function(vol_microm3, is_diatom) {
   if (is_diatom) {
-    pgC <- 0.288 * vol^0.811
+    pgC <- 0.288 * vol_microm3^0.811
   } else {
-    pgC <- 0.216 * vol^0.939
+    pgC <- 0.216 * vol_microm3^0.939
   }
-  return(pgC * cell_conc)
+  return(pgC)
 }
 
 # ---------------------
@@ -32,6 +45,9 @@ MendenDeuer_Lessard <- function(esd_microm, cell_conc, is_diatom) {
 
 # Merge Lafond diatoms data
 prof <- merge(prof, diatL, by = c("stn","depth"))
+
+# Correct units of detritus from ifcb
+prof$detritus_mg_L <- prof$detritus_mg_L / 1000
 
 # Remove stations where microscopy counts not done
 prof <- prof[,grep("NA",names(prof), invert = T)]
@@ -47,7 +63,7 @@ prof$scm[prof$depth < 10] <- 'surface'
 
 # Rename DMS and remove unnecessary variables
 prof$dms <- prof$dms_consens_cf68
-prof <- select(prof, c(stn,depth,cast,year,month,day,scm,dmspt,diat_cen,diat_pen,dino_athec,dino_thec,chrys,crypt,Phaeo,flag,diat_Laf.mgC_L.mic))
+prof <- select(prof, c(stn,depth,cast,year,month,day,scm,dmspt,diat_cen,diat_pen,dino_athec,dino_thec,chrys,crypt,Phaeo,flag,diat_Laf.mgC_L.mic,pras,prym_clumped_mg_L,detritus_mg_L))
 
 # ---------------------
 # Convert phyto counts to DMSP to estimate contribution
@@ -61,7 +77,7 @@ fp <- 0.9
 qdmsp.phaeo <- 10                                                 # fmol DMSP / cell
 prof$dmspt.Phaeo.counts <- prof$Phaeo * qdmsp.phaeo * 1e-6        # DMSPt-Phaeo
 prof$dmspt.Phaeo.counts.pc <- 100 * prof$dmspt.Phaeo.counts / (fp * prof$dmspt)
-print(summary(prof$dmspt.Phaeo.counts.pc))
+# print(summary(prof$dmspt.Phaeo.counts.pc))
 
 # # Using Phaeocystis carbon from iFCB: WEIRD
 # rdmsp.phaeo <- 0.05
@@ -69,7 +85,7 @@ print(summary(prof$dmspt.Phaeo.counts.pc))
 # prof$dmspt.Phaeo.Cifcb.pc <- 100*prof$dmspt.Phaeo.Cifcb/prof$dmspt
 # print(summary(prof$dmspt.Phaeo.Cifcb.pc))
 
-# Using carbon biomass based on cell counts and ESD estimated from abundance-weighted average of size intervals (see Taxonomy xls v2018)
+# Using biomass based on cell counts and ESD estimated from abundance-weighted average of size intervals (see Taxonomy xls v2018)
 esd <- list(dino_athec = 15,
             dino_thec = 15,
             diat_cen = 20,
@@ -77,48 +93,112 @@ esd <- list(dino_athec = 15,
             chrys = 10,
             crypt = 5,
             Phaeo = 5,
-            flag = 5)
-rdmsp <- list(dino_athec = 0.05,
-              dino_thec = 0.05,
+            flag = 5,
+            pras = 14)
+# # C_dmsp:C_tot ratio, optimized
+rdmsp <- list(dino_athec = 0.10,
+              dino_thec = 0.10,
               diat_cen = 0.01,
               diat_pen = 0.01,
               chrys = 0.10,
               crypt = 0.02,
-              Phaeo = 0.10,
-              flag = 0.10,
-              diat_all = 0.01)
+              Phaeo = 0.05,
+              flag = 0.05,
+              diat_all = 0.01,
+              pras = 0.025,
+              detritus = 0.01)
+if (Stefels == "_Stefels"){
+# C_dmsp:C_tot ratio, Stefels2007
+rdmsp <- list(dino_athec = 0.11,
+              dino_thec = 0.11,
+              diat_cen = 0.004,
+              diat_pen = 0.004,
+              chrys = 0.094,
+              crypt = 0.02,
+              Phaeo = 0.05,
+              flag = 0.05,
+              diat_all = 0.004,
+              pras = 0.025,
+              detritus = 0.01)}
+# Intracellular DMSP conc millimolar. Now using rdmsp * 2000
+cdmsp <- list(dino_athec = 200, 
+              dino_thec = 200,
+              diat_cen = 20,
+              diat_pen = 20,
+              chrys = 200,
+              crypt = 40,
+              Phaeo = 100,
+              flag = 100,
+              diat_all = 20,
+              pras = 50)
 
 for (nn in names(esd)) {
-  if (nn %in% c("diat_cen","diat_pen")) {
-    tmp <- MendenDeuer_Lessard(esd_microm = esd[[nn]], cell_conc = prof[[nn]], is_diatom = T) * 1e-9
-  } else {
-    tmp <- MendenDeuer_Lessard(esd_microm = esd[[nn]], cell_conc = prof[[nn]], is_diatom = F) * 1e-9
+  
+  vol_microm3 <- (4/3) * pi * ( (esd[[nn]] /2)^3 ) # µm^3
+  
+  if (cmethod == "ratio") {
+    
+    # Option A: from cell carbon and C_dmsp:C_tot ratio, Menden-Deuer and Lessard
+    if (nn %in% c("diat_cen","diat_pen")) {
+      tmp <- MendenDeuer_Lessard(vol_microm3, is_diatom = T) * 1e-9
+    } else {
+      tmp <- MendenDeuer_Lessard(vol_microm3, is_diatom = F) * 1e-9
+    }
+    prof[[paste(nn,"mgC_L.mic",sep = ".")]] <- tmp * prof[[nn]]
+    prof[[paste("dmspt",nn,"Cmic",sep = ".")]] <- tmp * prof[[nn]] * rdmsp[[nn]] * (1/12) * (1/5) * 1e6
+    prof[[paste("dmspt",nn,"Cmic.pc",sep = ".")]] <- 100 * tmp * prof[[nn]] * rdmsp[[nn]] * (1/12) * (1/5) * 1e6 / (fp * prof$dmspt)
+    
+  } else if (cmethod == "intraconc") {
+    
+    # Option B: from cell volume
+    prof[[paste("dmspt",nn,"Cmic",sep = ".")]] <- vol_microm3 * prof[[nn]] * cdmsp[[nn]] * (1e6 / 1e15)
+    prof[[paste("dmspt",nn,"Cmic.pc",sep = ".")]] <- 100 * vol_microm3 * prof[[nn]] * cdmsp[[nn]] * (1e6 / 1e15) / (fp * prof$dmspt)
   }
-  prof[[paste(nn,"mgC_L.mic",sep = ".")]] <- tmp
-  prof[[paste("dmspt",nn,"Cmic",sep = ".")]] <- tmp * rdmsp[[nn]] * (1/12) * (1/5) * 1e6
-  prof[[paste("dmspt",nn,"Cmic.pc",sep = ".")]] <- 100 * tmp * rdmsp[[nn]] * (1/12) * (1/5) * 1e6 / (fp * prof$dmspt)
 }
 
-# # Compare Lafond estimates and ESD-based estimates. ESD of 20 for both centric and pennates gives good approximation
-# plot(prof$diat_Laf.mgC_L.mic, prof$diat_cen.mgC_L.mic + prof$diat_pen.mgC_L.mic,
-#      main = paste0("r = ",cor(prof$diat_Laf.mgC_L.mic, prof$diat_cen.mgC_L.mic + prof$diat_pen.mgC_L.mic, "pairwise.complete.obs", method = "s")))
+if (cmethod == "ratio") {
+  # # Compare Lafond estimates and ESD-based estimates. ESD of 20 for both centric and pennates gives good approximation
+  # plot(prof$diat_Laf.mgC_L.mic, prof$diat_cen.mgC_L.mic + prof$diat_pen.mgC_L.mic,
+  #      main = paste0("r = ",cor(prof$diat_Laf.mgC_L.mic, prof$diat_cen.mgC_L.mic + prof$diat_pen.mgC_L.mic, "pairwise.complete.obs", method = "s")))
+}
+
+# Remove microscopy counts-based data for diatoms
+if (cmethod == "ratio") {
+  prof <- select(prof, -c(diat_cen.mgC_L.mic, dmspt.diat_cen.Cmic, dmspt.diat_cen.Cmic.pc, diat_pen.mgC_L.mic, dmspt.diat_pen.Cmic, dmspt.diat_pen.Cmic.pc))
+} else if (cmethod == "intraconc") {
+  prof <- select(prof, -c( dmspt.diat_cen.Cmic, dmspt.diat_cen.Cmic.pc, dmspt.diat_pen.Cmic, dmspt.diat_pen.Cmic.pc))
+}
 
 # Add DMSP for Lafond diatoms
 prof$dmspt.diat_Laf.Cmic <- prof$diat_Laf.mgC_L.mic * rdmsp$diat_all * (1/12) * (1/5) * 1e6
 prof$dmspt.diat_Laf.Cmic.pc <- 100 * prof$dmspt.diat_Laf.Cmic / (fp * prof$dmspt)
 
-# Remove microscopy counts-based data for diatoms
-prof <- select(prof, -c(diat_cen.mgC_L.mic, dmspt.diat_cen.Cmic, dmspt.diat_cen.Cmic.pc, diat_pen.mgC_L.mic, dmspt.diat_pen.Cmic, dmspt.diat_pen.Cmic.pc))
+if (addcd == "_PhaeoCol_detr"){
+  # Add DMSP for colonies "clumped prym"
+  prof$dmspt.Phaeo_col.ifcb <- (prof$prym_clumped_mg_L / 10) * rdmsp$Phaeo * (1/12) * (1/5) * 1e6
+  prof$dmspt.Phaeo_col.ifcb.pc <- 100 * prof$dmspt.Phaeo_col.ifcb / (fp * prof$dmspt)
+  # Add DMSP for detritus
+  prof$dmspt.detritus.ifcb <- prof$detritus_mg_L * rdmsp$detritus * (1/12) * (1/5) * 1e6 # NOTE: units corrected from mg to ugC/L (above)
+  prof$dmspt.detritus.ifcb.pc <- 100 * prof$dmspt.detritus.ifcb / (fp * prof$dmspt)
+  # Increase maximum y axis DMSP
+  ymax <- 250
+}
 
 # OUTPUT
 OUT <- prof
 # View(OUT)
 write.csv(x = OUT, file = paste0(opath,"Fraction_DMSPt_phyto.csv"), row.names = F)
 
-# ---------------------
+
+# ---------------------------------------------------------------
 # Plot
 
-parcol <- brewer.pal(8, 'Dark2')
+# Bar color scheme
+if (addcd == "_PhaeoCol_detr"){
+  phycol <- brewer.pal(12, 'Paired')
+} else {
+  phycol <- brewer.pal(8, 'Dark2')[c(1,2,3,4,5,7,8,6)]
+}
 
 phynames <- list("dmspt.diat_Laf.Cmic.pc"="Diat_all",
                  "dmspt.diat_cen.Cmic.pc"="Diat_C",
@@ -129,7 +209,10 @@ phynames <- list("dmspt.diat_Laf.Cmic.pc"="Diat_all",
                  "dmspt.crypt.Cmic.pc"="Crypto",
                  "dmspt.Phaeo.Cmic.pc"="Phaeocystis",
                  "dmspt.Phaeo.counts.pc"="Phaeocystis_cq",
-                 "dmspt.flag.Cmic.pc"="Flag_other")
+                 "dmspt.flag.Cmic.pc"="Flag_other",
+                 "dmspt.Phaeo_col.ifcb.pc"="Phae_col",
+                 "dmspt.detritus.ifcb.pc"="Detritus",
+                 "dmspt.pras.Cmic.pc"="Prasino")
 dfplot <-prof[ , c( "stn","depth","scm","dmspt",names(prof)[grepl("dmspt.", names(prof)) & grepl(".pc", names(prof))] )]
 for (nn in names(phynames)) {names(dfplot)[names(dfplot) == nn] <- phynames[[nn]]}
 
@@ -138,12 +221,11 @@ for (nn in names(phynames)) {names(dfplot)[names(dfplot) == nn] <- phynames[[nn]
 #                    'surface' = rowSums(dfplot[dfplot$scm=="surface", 6:dim(dfplot)[2]]),
 #                    'SCM' = rowSums(dfplot[dfplot$scm=="SCM", 6:dim(dfplot)[2]]))
 # View(sums)
-
-# # TidyR format
+# # TidyR format: not needed if using base barplot
 # gather(prof[ ,grepl("dmspt.", names(prof)) & !grepl(".pc", names(prof))])
 
 
-if (exportimg) {png(filename = paste0(opath,"dmspt_biomass.png"), width = 16, height = 12, units = 'cm', pointsize = 8, bg = 'white', res = 600, type = 'cairo')}
+if (exportimg) {png(filename = paste0(opath,"dmspt_biomass_",cmethod,addcd,Stefels,".png"), width = 17, height = 12, units = 'cm', pointsize = 8, bg = 'white', res = 600, type = 'cairo')}
 
 # Multipanel setup
 m <- rbind(matrix(data = 1, nrow = 4, ncol = 9), matrix(data = 2, nrow = 4, ncol = 9))
@@ -159,11 +241,11 @@ barplot(height = t(as.matrix(dfplot[dfplot$scm=="surface",6:dim(dfplot)[2]])),
         beside = F,
         legend.text = names(dfplot)[6:dim(dfplot)[2]],
         args.legend = list(x = "right", bty = "n", cex = 1.2),
-        col = parcol,
+        col = phycol,
         border = F,
         ylab = "% of DMSPp",
         axes = F,
-        ylim = c(0,100),
+        ylim = c(0,ymax),
         cex = 1.2,
         cex.names = 1.2,
         cex.lab = 1.2,
@@ -184,11 +266,11 @@ par(mar = c(5,5,2,2))
 barplot(height = t(as.matrix(dfplot[dfplot$scm=="SCM",6:dim(dfplot)[2]])),
         names.arg = dfplot$stn[dfplot$scm=="SCM"],
         beside = F,
-        col = parcol,
+        col = phycol,
         border = F,
         ylab = "% of DMSPp",
         axes = F,
-        ylim = c(0,100),
+        ylim = c(0,ymax),
         cex = 1.2,
         cex.names = 1.2,
         cex.lab = 1.2,
